db6ea6769c0a6aa6043e6e1f436250e541a25f41
Timothy Hoogland <houseofbugs@gmail.com>
2018-10-16 18:49:32 -0500

    More updates for upsming U1.R2.4 release
    More updates for upsming U1.R2.4 release

--- TH3DUF_R2/ultralcd.cpp
+++ TH3DUF_R2/ultralcd.cpp
@@ -3816,7 +3816,7 @@ void lcd_quick_feedback(const bool clear_buttons) {
     #elif HAS_BED_PROBE
       MENU_ITEM_EDIT(float52, MSG_ZPROBE_ZOFFSET, &zprobe_zoffset, Z_PROBE_OFFSET_RANGE_MIN, Z_PROBE_OFFSET_RANGE_MAX);
     #endif
-
+    
     #if DISABLED(SLIM_LCD_MENUS)
 
       // M203 / M205 - Feedrate items
