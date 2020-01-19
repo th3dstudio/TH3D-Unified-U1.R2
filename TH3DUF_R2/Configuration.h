63f46e7c4d179a744e2669a4939f072003ae2b13
Timothy Hoogland <houseofbugs@gmail.com>
2020-01-19 01:19:29 -0600

    Update Configuration.h

--- TH3DUF_R2/Configuration.h
+++ TH3DUF_R2/Configuration.h
@@ -1051,6 +1051,6 @@
 #include "Configuration_beta.h"
 #include "Configuration_backend.h"
 
-#define UNIFIED_VERSION "TH3D U1.R3.1"
+#define UNIFIED_VERSION "TH3D U1.R2.23"
 
 #endif // CONFIGURATION_H
